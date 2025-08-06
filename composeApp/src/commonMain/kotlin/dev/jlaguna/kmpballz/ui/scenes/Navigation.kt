6b315d2ed409c82d87a0ad5c9a1f92ef82d33767
Javier Laguna <javier.laguna@innocv.com>
2025-08-06 19:11:28 +0200

    Update to navigation-compose = "2.9.0-beta04"

--- composeApp/src/commonMain/kotlin/dev/jlaguna/kmpballz/ui/scenes/Navigation.kt
+++ composeApp/src/commonMain/kotlin/dev/jlaguna/kmpballz/ui/scenes/Navigation.kt
@@ -1,38 +1,46 @@
 package dev.jlaguna.kmpballz.ui.scenes
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import dev.jlaguna.kmpballz.ui.scenes.characterDetail.CharacterDetailScreen
 import dev.jlaguna.kmpballz.ui.scenes.charactersList.CharactersListScreen
+import kotlinx.serialization.Serializable
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.core.parameter.parametersOf
+
+private object Routes {
+    @Serializable
+    object CharactersList
+
+    @Serializable
+    data class CharacterDetail(val characterId: Int)
+}
 
 @OptIn(KoinExperimentalAPI::class)
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "home") {
-        composable("home") {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.CharactersList
+    ) {
+        composable<Routes.CharactersList> {
             CharactersListScreen(
                 onCharacterClick = { character ->
-                    navController.navigate("detail/${character.id}")
+                    navController.navigate(Routes.CharacterDetail(characterId = character.id))
                 }
             )
         }
 
-        composable(
-            "detail/{characterId}",
-            arguments = listOf(navArgument("characterId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val characterId = checkNotNull(backStackEntry.arguments?.getInt("characterId"))
+        composable<Routes.CharacterDetail> { backStackEntry ->
+            val route = backStackEntry.toRoute<Routes.CharacterDetail>()
             CharacterDetailScreen(
-                vm = koinViewModel(parameters = { parametersOf(characterId) }),
+                vm = koinViewModel(parameters = { parametersOf(route.characterId) }),
                 onBack = { navController.popBackStack() }
             )
         }
